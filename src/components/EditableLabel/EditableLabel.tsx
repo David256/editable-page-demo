7447af0513d4face3abed2e4d8b1e0e47dbb1cef
David256 <wasterdavid@gmail.com>
2022-05-17 22:46:21 -0500

    WIP: working on building the EditableLabel component

---
+++ src/components/EditableLabel/EditableLabel.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import './EditableLabel.sass';
+
+export interface EditableLabelProps {
+  text: string,
+  save: (text: string) => void,
+  editable?: boolean,
+};
+
+export function EditableLabel(props: EditableLabelProps) {
+  const {
+    text,
+    save,
+    editable=false,
+  } = props;
+
+  const [className, setClassName] = useState<string>('EditableLabel');
+
+  useEffect(() => {
+    let newClassName = 'EditableLabel';
+    if (editable) {
+      newClassName = newClassName.concat(' Editable');
+    }
+    setClassName(newClassName);
+    console.log('change className to', newClassName);
+  }, [editable]);
+
+  return (
+    <div className={className}>
+      {text}
+    </div>
+  );
+}
