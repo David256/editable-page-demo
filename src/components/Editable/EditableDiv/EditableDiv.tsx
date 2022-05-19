a8b36c2ba31e706317792ac8abade378e2a7bfb1
David256 <wasterdavid@gmail.com>
2022-05-18 19:21:48 -0500

    Create new editable field

---
+++ src/components/Editable/EditableDiv/EditableDiv.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+import { Editable } from '../Editable';
+
+import './EditableDiv.sass';
+
+export interface EditableDivProps {
+  width?: number,
+  height?: number,
+  html: string,
+  save: (text: string) => void,
+};
+
+export function EditableDiv(props: EditableDivProps) {
+  const {
+    width=10,
+    height=10,
+    html,
+    save,
+  } = props;
+
+  const [isEditable, setIsEditable] = useState(false);
+  const [currentHTML, setCurrentHTML] = useState(html);
+  const ref = useRef<HTMLDivElement>();
+
+  // If `currentHTML` change, we save that value
+  useEffect(() => {
+    save(currentHTML);
+  }, [currentHTML]);
+
+  const onSave = () => {
+    console.log('saving...', currentHTML);
+    if (ref.current) {
+      const current = ref.current as unknown as HTMLDivElement;
+      console.log(current);
+      setCurrentHTML(current.innerHTML);
+    }
+    setIsEditable(false);
+  }
+
+  return (
+    <Editable
+      editable={isEditable}
+      onEditable={setIsEditable}
+    >
+      <div
+        style={{
+          width: `${width}px`,
+          height: `${height}px`,
+        }}
+        ref={ref as unknown as React.LegacyRef<HTMLDivElement>}
+        className="EditableDiv"
+        contentEditable={isEditable}
+        onBlur={onSave}
+        onKeyDown={(e) => e.key === 'Escape' && onSave()}
+        dangerouslySetInnerHTML={{__html: currentHTML}}
+        suppressContentEditableWarning={true}
+      ></div>
+    </Editable>
+  );
+}
